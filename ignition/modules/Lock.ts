c96321a2edb96f88535b01a9248b88ef09710f45
Aqrare <takumitakumi0310@gmail.com>
2024-09-02 19:06:00 +0900

    create ERC1155 contract

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const LockModule = buildModule("LockModule", (m) => {
 
-  const lock = m.contract("Lock", [
+  const lock = m.contract("YUZUERC1155", [
     "0x65150B5Fa861481651225Ef4412136DCBf696232",
   ]);
 
