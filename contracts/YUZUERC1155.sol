6e5c5c80d7f6736ce92642ce72fb96cc5d540b0e
Aqrare <takumitakumi0310@gmail.com>
2024-09-11 18:04:23 +0900

    add metadata

--- contracts/YUZUERC1155.sol
+++ contracts/YUZUERC1155.sol
@@ -6,20 +6,52 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract YUZUERC1155 is ERC1155, Ownable {
-    constructor(address initialOwner)
-        ERC1155("https://pbs.twimg.com/profile_images/1517210325707370496/vqYXWuI1_400x400.jpg")
+    uint256 public constant MAX_NFT_ID = 16;
+    uint256 public constant MIN_NFT_ID = 1;
+
+    constructor(
+        address initialOwner
+    )
+        ERC1155(
+            "https://pbs.twimg.com/profile_images/1517210325707370496/vqYXWuI1_400x400.jpg"
+        )
         Ownable(initialOwner)
     {}
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-    {
+    function drawGacha() external {
+        uint256 randomId = _getPseudoRandomNFTId();
+        _mint(msg.sender, randomId, 1, "");
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-    {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public {
         _mintBatch(to, ids, amounts, data);
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function _getPseudoRandomNFTId() internal view returns (uint256) {
+        uint256 blockHashValue = uint256(blockhash(block.number - 1));
+        uint256 random = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender,blockHashValue)
+            )
+        ) % MAX_NFT_ID;
+        return random;
     }
 }
