67b28333b444755b52fd1dcc96d146c2dcd63a56
Aqrare <takumitakumi0310@gmail.com>
2024-11-29 12:46:39 +0900

    add script

--- script/DeployERC1155.s.sol
+++ script/DeployERC1155.s.sol
@@ -9,7 +9,43 @@ contract DeployERC1155 is Script {
         vm.startBroadcast();
         address ADMIN_WALLET_ADDRESS = 0x2C0d48DD817DeEaD079A814A67aF406Bce5B08dc;
         TestERC1155 testERC1155 = new TestERC1155(ADMIN_WALLET_ADDRESS);
-        console.log("Showtie ERC20 deployed at:", address(testERC1155));
+        console.log("ERC1155 deployed at:", address(testERC1155));
         vm.stopBroadcast();
     }
 }
+
+contract MintERC1155 is Script {
+    function run() public {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address recipient = 0x65150B5Fa861481651225Ef4412136DCBf696232;
+
+        address tokenAddress = 0xBA4CD2a4c6B87BA9D245b0c8824c77f263807f68;
+        TestERC1155 token = TestERC1155(tokenAddress);
+
+        vm.startBroadcast(privateKey);
+        uint256 tokenId = 10101;
+        uint256 amount = 1;
+
+        // mint関数を呼び出す
+        token.mint(recipient, tokenId, amount);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract BalanceOfERC1155 is Script {
+    function run() public {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address tokenAddress = 0xBA4CD2a4c6B87BA9D245b0c8824c77f263807f68;
+        vm.startBroadcast(privateKey);
+
+        TestERC1155 token = TestERC1155(tokenAddress);
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = 10101;
+        address[] memory accounts = new address[](1);
+        accounts[0] = 0x65150B5Fa861481651225Ef4412136DCBf696232;
+        uint256[] memory balances = token.balanceOfBatch(accounts, tokenIds);
+        console.log(balances[0]);
+        vm.stopBroadcast();
+    }}
+
